24442af49d7580fcd1611d893a7ca618b3ea373a
Leonardo Camelo <leonardocamelo.nave@gmail.com>
2017-05-18 22:23:51 -0300

    Game specs

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -1,5 +1,25 @@
 describe TicTacToe::Game do
+  let :game do
+    TicTacToe::Game.new
+  end
+
   describe '#run' do
-    it 'print instructions and perform the game loop'
+    before do
+      allow(STDOUT).to receive(:puts)
+      allow_any_instance_of(TicTacToe::GameLoop).to receive(:perform)
+    end
+
+    it 'prints the game title' do
+      expect { game.run }.to output(/Tic-Tac-Toe/).to_stdout
+    end
+
+    it 'performs the game loop' do
+      expect_any_instance_of(TicTacToe::GameLoop).to receive(:perform).once
+      game.run
+    end
+
+    it 'prints the game over message' do
+      expect { game.run }.to output(/Game over/).to_stdout
+    end
   end
 end
